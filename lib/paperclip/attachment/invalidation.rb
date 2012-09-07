8d6f3a8e51306a86511026e32489f1ddcae06c16
John Tjanaka <zonecheung@gmail.com>
2012-09-07 06:15:22 -0500

    Initial commit for paperclip invalidation.

---
+++ lib/paperclip/attachment/invalidation.rb
@@ -0,0 +1,48 @@
+require "net/http"
+
+module Paperclip
+  module Attachment
+
+    class Invalidation
+
+      def initialize(url, settings = {})
+        @url = url
+
+        @cache_invalidation_url        = settings[:cache_invalidation_url]
+        @cache_invalidation_username   = settings[:cache_invalidation_username] || ENV["CACHE_INVALIDATION_USERNAME"]
+        @cache_invalidation_password   = settings[:cache_invalidation_password] || ENV["CACHE_INVALIDATION_PASSWORD"]
+        @cache_invalidation_param_name = settings[:cache_invalidation_param_name] || "filename"
+        
+        @cloudflare_file_purge_url     = settings[:cloudflare_file_purge_url] || ENV["CLOUDFLARE_FILE_PURGE_URL"] || "https://www.cloudflare.com/api_json.html"
+        @cloudflare_tkn                = settings[:cloudflare_tkn] || ENV["CLOUDFLARE_TKN"]
+        @cloudflare_email              = settings[:cloudflare_email] || ENV["CLOUDFLARE_EMAIL"]
+        @host                          = settings[:host] || ENV["HOST"].to_s.sub(/^www\./, "")
+      end
+
+      def invalidate_on_asset_host
+        uri = URI.parse(@cache_invalidation_url)
+        req = Net::HTTP::Post.new(uri.path)
+        req.basic_auth(@cache_invalidation_username,
+                       @cache_invalidation_password) if @cache_invalidation_username.present?
+        req.set_form_data(@cache_invalidation_param_name => URI.parse(@url).path)
+        Net::HTTP.new(uri.host, uri.port).start { |http| http.request(req) }
+      end
+
+      def invalidate_on_cloudflare
+        uri = URI.parse(@cloudflare_file_purge_url)
+        req = Net::HTTP::Post.new(uri.path)
+        req.set_form_data("a"     => "zone_file_purge",
+                          "z"     => @host,
+                          "tkn"   => @cloudflare_tkn,
+                          "email" => @cloudflare_email,
+                          "url"   => @url)
+        net = Net::HTTP.new(uri.host, uri.port)
+        net.use_ssl = !! (@cloudflare_file_purge_url =~ /^https/)
+        net.start { |http| http.request(req) }
+      end
+
+    end
+
+  end
+end
+
---
+++ paperclip_invalidation.gemspec
@@ -0,0 +1,21 @@
+Gem::Specification.new do |s|
+  s.name                      = "paperclip_invalidation"
+  s.version                   = "0.0.1"
+  s.platform                  = Gem::Platform::RUBY
+  s.authors                   = [ "John Tjanaka" ]
+  s.email                     = [ "zonecheung@gmail.com" ]
+  s.homepage                  = "https://github.com/zonecheung"
+  s.description               = "Invalidate paperclip attachment in cloudflare or an external host"
+  s.summary                   = "paperclip_invalidation-#{s.version}"
+
+  s.rubyforge_project         = "paperclip_invalidation"
+  s.required_rubygems_version = "> 1.3.6"
+
+  s.add_dependency "activesupport" , ">=3.0.0"
+  s.add_dependency "rails"         , ">=3.0.0"
+
+  s.files = `git ls-files`.split("\n")
+  s.executables = `git ls-files`.split("\n").map{|f| f =~ /^bin\/(.*)/ ? $1 : nil}.compact
+  s.require_path = 'lib'
+end
+
