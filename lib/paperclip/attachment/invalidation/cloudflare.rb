ab4040615a8780eabab7dc5e8e4f707611a8bc07
John Tjanaka <zonecheung@gmail.com>
2016-03-02 03:55:03 -0500

    Update the code to cloudflare API v4. [#114776087]

--- lib/paperclip/attachment/invalidation/cloudflare.rb
+++ lib/paperclip/attachment/invalidation/cloudflare.rb
@@ -5,28 +5,67 @@ module Paperclip
     class Invalidation
       class Cloudflare
     
-        def initialize(url, settings = {})
-          @url = url
-          
-          @file_purge_url = settings[:url] || ENV["CLOUDFLARE_FILE_PURGE_URL"] || "https://www.cloudflare.com/api_json.html"
-          @tkn            = settings[:tkn] || ENV["CLOUDFLARE_TKN"]
-          @email          = settings[:email] || ENV["CLOUDFLARE_EMAIL"]
-          @host           = settings[:host] || ENV["HOST"].to_s.sub(/^www\./, "")
+        API_URL = "https://api.cloudflare.com/client/v4"
+
+        def initialize(options = {})
+          @host  = options[:host]
+          @email = options[:email]
+          @tkn   = options[:tkn]
         end
 
-
-        def invalidate
-          uri = URI.parse(@file_purge_url)
-          req = Net::HTTP::Post.new(uri.path)
-          req.set_form_data("a"     => "zone_file_purge",
-                            "z"     => @host,
-                            "tkn"   => @tkn,
-                            "email" => @email,
-                            "url"   => @url)
-          net = Net::HTTP.new(uri.host, uri.port)
-          net.use_ssl = !! (@file_purge_url =~ /^https/)
-          net.start { |http| http.request(req) }
+        def purge_all_files
+          get_zone_ids.inject([]) { |results, id| results << purge_cache(id, purge_everything: true) }
         end
+
+        def purge_individual_files(files = [])
+          get_zone_ids.inject([]) { |results, id| results << purge_cache(id, files: files) }
+        end
+
+        private
+
+        def get_zone_ids
+          uri = URI("#{API_URL}/zones?name=#{@host}")
+
+          Net::HTTP.start(
+            uri.host, uri.port,
+            use_ssl: uri.scheme == "https"
+          ) do |http|
+            request = Net::HTTP::Get.new uri
+
+            request["X-Auth-Email"] = @email
+            request["X-Auth-Key"] = @tkn
+            request["Content-Type"] = "application/json"
+
+            response = http.request request # Net::HTTPResponse object
+            if response.is_a?(Net::HTTPSuccess)
+              json = JSON.parse(response.body)
+              json["result"].map { |x| x["id"] }
+            else
+              raise response.message.inspect
+            end
+          end      
+        end
+
+        def purge_cache(id, hash)
+          uri = URI("#{API_URL}/zones/#{id}/purge_cache")
+
+          Net::HTTP.start(
+            uri.host, uri.port,
+            use_ssl: uri.scheme == "https"
+          ) do |http|
+            request = Net::HTTP::Delete.new uri
+
+            request["X-Auth-Email"] = @email
+            request["X-Auth-Key"] = @tkn
+            request["Content-Type"] = "application/json"
+
+            request.body = hash.to_json
+
+            response = http.request request # Net::HTTPResponse object
+            JSON.parse(response.body)
+          end      
+        end
+
     
       end
     end
@@ -36,3 +75,14 @@ end
 
 
 
+require "net/http"
+
+# NOTE: I'm not sure where to name this file/lib yet. :(
+module Modules
+
+  class Cloudflare
+
+
+  end
+
+end
